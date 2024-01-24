d4a5162062f2f5e5c5f12d3ac461022aa53db170
Shane Lillie <ignignokterr@gmail.com>
2024-01-23 19:03:46 -0800

    Lobby into level, nice

---
+++ scripts/Levels/Lobby.cs
@@ -0,0 +1,39 @@
+using Godot;
+
+using pdxpartyparrot.ggj2024.Managers;
+
+namespace pdxpartyparrot.ggj2024.Levels
+{
+    public partial class Lobby : Node
+    {
+        [Export]
+        private PackedScene _gameLevel;
+
+        [Export]
+        private Label _playerCount;
+
+        #region Godot Lifecycle
+
+        public override void _Ready()
+        {
+            _playerCount.Text = "TODO";
+        }
+
+        public override void _Process(double delta)
+        {
+            // TODO: update this in an event handler, not here
+            _playerCount.Text = "TODO";
+        }
+
+        #endregion
+
+        #region Signal Handlers
+
+        private async void _on_start_pressed()
+        {
+            await LevelManager.Instance.LoadLevelAsync(_gameLevel).ConfigureAwait(false);
+        }
+
+        #endregion
+    }
+}
